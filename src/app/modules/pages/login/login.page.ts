999f107a417d48e4a145592bd4af9a7d60c3d99d
Sergio Burgueño <burguer@gmail.com>
2020-07-29 22:27:03 -0700

    linter fixes

--- src/app/modules/pages/login/login.page.ts
+++ src/app/modules/pages/login/login.page.ts
@@ -1,14 +1,14 @@
 import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 import * as localizedStrings from '../../core/consts/localized-strings.const';
 import {AuthService} from '../../core/services/auth.service';
 import {NavigationService} from '../../core/services/navigation.service';
-import {PhoneNumber} from "../../../shared/models/phone-number.model";
-import {RegEx} from "../../core/consts/regex.const";
-import {IonInput} from "@ionic/angular";
-import {CountdownComponent} from "ngx-countdown";
-import {ErrorService} from "../../core/services/error.service";
+import {PhoneNumber} from '../../../shared/models/phone-number.model';
+import {RegEx} from '../../core/consts/regex.const';
+import {IonInput} from '@ionic/angular';
+import {CountdownComponent} from 'ngx-countdown';
+import {ErrorService} from '../../core/services/error.service';
 
 @Component({
     selector: 'app-login',
@@ -16,8 +16,8 @@ import {ErrorService} from "../../core/services/error.service";
     styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements AfterViewInit, OnInit {
-    @ViewChild("phoneLineInput", {static: false}) phoneLineInputField: IonInput;
-    @ViewChild("confirmationCodeInput", {static: false}) confirmationCodeInputField: IonInput;
+    @ViewChild('phoneLineInput', {static: false}) phoneLineInputField: IonInput;
+    @ViewChild('confirmationCodeInput', {static: false}) confirmationCodeInputField: IonInput;
     @ViewChild('cd', {static: false}) private countdown: CountdownComponent;
     confirmationResult = this.authService.confirmationResult;
     countdownSeconds = 120;
@@ -36,19 +36,19 @@ export class LoginPage implements AfterViewInit, OnInit {
                 private formBuilder: FormBuilder,
                 private navigationService: NavigationService) {
         this.stepOne = this.formBuilder.group({
-            phoneLine: new FormControl("", Validators.compose([
+            phoneLine: new FormControl('', Validators.compose([
                 Validators.required,
                 Validators.minLength(16),
             ]))
-        })
+        });
 
         this.stepTwo = this.formBuilder.group({
-            confirmationCode: new FormControl("", Validators.compose([
+            confirmationCode: new FormControl('', Validators.compose([
                 Validators.required,
                 Validators.minLength(6),
                 Validators.pattern(RegEx.NUMERIC)
             ]))
-        })
+        });
     }
 
     async ngAfterViewInit() {
@@ -66,7 +66,7 @@ export class LoginPage implements AfterViewInit, OnInit {
     async finishCountDown(e) {
         if (e.action === 'done') {
             await this.authService.logOut();
-            this.showFirstStep()
+            this.showFirstStep();
         }
     }
 
@@ -124,7 +124,7 @@ export class LoginPage implements AfterViewInit, OnInit {
             this.showProgressBar = true;
             this.authService.verifyOTP(this.stepTwo.get('confirmationCode').value, this.confirmationResult).then(() => {
                 this.navigationService.navigateToDashboard();
-                this.showFirstStep()
+                this.showFirstStep();
             }).catch((error) => {
                 this.handleError(error);
                 this.showProgressBar = false;
